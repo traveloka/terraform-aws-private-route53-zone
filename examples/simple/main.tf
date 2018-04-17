8e158cad307d7b0e29b11cbe7fa65aadf88f6ee7
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2018-04-17 16:24:34 +0700

    remove description from example

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -5,7 +5,6 @@ module "test" {
   main_vpc       = "vpc-12345678"
   secondary_vpcs = ["vpc-fedbca09", "vpc-abcdef12"]
 
-  description    = "Flight Product route53 hosted zone"
   environment    = "production"
   product_domain = "fpr"
 }
